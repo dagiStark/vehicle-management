0240ad5f9d5b046a4a73dac2fa1f3a7ef9283f46
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-14 15:58:19 +0300

    Update Main.jsx

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,11 +1,39 @@
-import React from "react";
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { CalendarDays, Search } from "lucide-react";
+import { Input } from "@mui/material";
+
+const DateRangeDisplay = () => {
+  const today = new Date();
+  const formattedDate = today.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <div className="flex items-center space-x-2 px-4 py-2 rounded-full border border-gray-300 shadow-sm bg-white hover:shadow-md transition">
+      <CalendarDays className="text-gray-500" /> {/* Calendar Icon */}
+      <span className="text-sm font-medium text-gray-700">{formattedDate}</span>
+    </div>
+  );
+};
 
 function Main() {
   return (
-    <div>
+    <div className="w-full h-full flex flex-col pt-3 ml-3">
       <header>
-        <h1>Vehicle Management</h1>
+        <div className="flex justify-between items-center mr-10">
+          <h1 className="text-3xl font-bold text-[#adde33]">Dagim Garage</h1>
+          <div className="flex items-center gap-10">
+            <div className="flex items-center gap-2">
+              <Search />
+              <Input placeholder="Search..." />
+            </div>
+            <div>
+              <DateRangeDisplay />
+            </div>
+          </div>
+        </div>
       </header>
       <main>
         <Outlet />
