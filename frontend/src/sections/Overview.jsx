b1f54fdc1bfbf09693b08f0b8c6025137b10e7ec
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-14 16:00:03 +0300

    added both left and right sections to overview

--- frontend/src/sections/Overview.jsx
+++ frontend/src/sections/Overview.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import LeftOverview from "../components/LeftOverview";
+import RightOverview from "../components/RightOverview";
 
 function Overview() {
   return (
-    <div>
-      Overview
+    <div className=" w-full">
+      <div className="flex gap-8 pl-5 pr-5 mt-5">
+        {/* Left Side */}
+        <div className="w-[80%]">
+          <LeftOverview />
+        </div>
+
+        {/* Right Side */}
+        <div className="=">
+          <RightOverview />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Overview
+export default Overview;
