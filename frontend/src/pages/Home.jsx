55780427dfb9a18ff85fa62e01d42726f3e00c20
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-15 11:33:47 +0300

    home page redirect updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Main from "../components/Main";
 import Sidebar from "../components/Sidebar";
@@ -12,6 +12,7 @@ function Home() {
         <Sidebar />
         <Routes>
           <Route path="/" element={<Main />}>
+            <Route index element={<Navigate to="overview" />} />
             <Route path="overview" element={<Overview />} />
             <Route path="vehicles" element={<Vehicles />} />
           </Route>
