2925e04474176cf9c99b8b83da97a3a5cd78e411
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-15 11:24:13 +0300

    fixed cors error and env variable

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const vehicleRoutes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/api/vehicles", vehicleRoutes);
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(MONGO_URI)
   .then(() =>
     app.listen(PORT, () => {
       console.log("Connected to MongoDB");
@@ -24,4 +27,3 @@ mongoose
   )
   .catch((err) => console.error(err));
 
-// Routes
